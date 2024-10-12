66ab0df539b4315ad8699ef1d5f1dba43474c0aa
kojikawazu <nanairo7.daydream21@gmail.com>
2024-10-12 13:28:17 +0900

    テストAPI修正

--- src/app/api/apitest/route.ts
+++ src/app/api/apitest/route.ts
@@ -1,4 +1,16 @@
+import { cookies } from 'next/headers';
+
 export async function GET() {
     console.log('API Route - getuser called');
+
+    const fetchUrl = `${process.env.API_URL}/users/auth-check`;
+    console.log('fetchUrl:', fetchUrl);
+
+    // クッキーを取得
+    const cookieStore = cookies();
+    const tokenCookie = cookieStore.get('token');
+    console.log('isTokenCookie:', tokenCookie !== undefined);
+    console.log('tokenCookie:', tokenCookie);
+
     return Response.json({ message: 'Hello from getuser' }, { status: 200 });
 }
