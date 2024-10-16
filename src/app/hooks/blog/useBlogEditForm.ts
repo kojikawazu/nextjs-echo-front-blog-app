0b0a299db4989fd435a8c53895e136a5e8636ad5
kojikawazu <nanairo7.daydream21@gmail.com>
2024-10-16 22:53:11 +0900

    ブログ編集カスタムhooks化

---
+++ src/app/hooks/blog/useBlogEditForm.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import { BlogCreateFormType } from '@/app/types/blogs-types';
+
+/**
+ * ブログ編集フォームカスタムフック
+ * @returns カスタムhooks
+ */
+export const useBlogEditForm = () => {
+    // ブログデータ取得中
+    const [isLoadingBlogData, setIsLoadingBlogData] = useState(true);
+    // フォームデータ
+    const [formData, setFormData] = useState<BlogCreateFormType>({
+        title: '',
+        description: '',
+        githubUrl: '',
+        category: '',
+        tags: '',
+    });
+
+    return {
+        isLoadingBlogData,
+        formData,
+        setIsLoadingBlogData,
+        setFormData,
+    };
+};
