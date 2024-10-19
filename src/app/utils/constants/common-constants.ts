9a3b52735ca689df03cd0e6083dad15df0434443
kojikawazu <nanairo7.daydream21@gmail.com>
2024-10-19 21:51:15 +0900

    ブログ詳細ページの調整

---
+++ src/app/utils/constants/common-constants.ts
@@ -0,0 +1,29 @@
+/**
+ * Common Constants
+ */
+export const CommonConstants = {
+    // toast messages
+    TOAST_MESSAGE: {
+        CONFIRM_BLOG_ADD: 'ブログを追加してもよろしいですか？',
+
+        CONFIRM_DELETE_BLOG: 'ブログを削除しますか？',
+        CONFIRM_ADD_COMMENT: 'コメントを追加しますか？',
+
+        ADD_COMMENT_INVALID_REQUIRED: '名前とコメントは必須です。',
+        ADD_COMMENT_SUCCESSED: 'コメントを追加しました。',
+    },
+    // URL path
+    URL_PATH: {
+        BLOG_HOME: '/blog',
+    },
+    // API error message
+    ERROR_MESSAGE: {
+        API_ROUTER_ERROR: 'API Error',
+
+        DEL_BLOG_FAILURE: 'ブログを削除しました。',
+
+        ADD_COMMENT_FAILURE: 'コメントの追加に失敗しました。',
+
+        BLOG_LIKE_FAILURE: 'いいねの登録/失敗に失敗しました。',
+    },
+};
